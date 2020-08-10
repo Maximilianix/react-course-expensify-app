c18d3c20bb2f23a5541a67d768780967f2de7863
Maxim <mreksha@gmail.com>
2020-08-10 13:42:31 +0300

    setup production web build

---
+++ dist/webpack.config.dev.js
@@ -0,0 +1,46 @@
+"use strict";
+
+var path = require('path');
+
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = function (env) {
+  var isProduction = env === 'production';
+  var CSSExtract = new ExtractTextPlugin('styles.css');
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: CSSExtract.extract({
+          use: [{
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          }, {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }]
+        })
+      }]
+    },
+    plugins: [CSSExtract],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
+    }
+  };
+};
+//# sourceMappingURL=webpack.config.dev.js.map
---
+++ src/components/dist/ExpenseForm.dev.js
@@ -0,0 +1,2 @@
+"use strict";
+//# sourceMappingURL=ExpenseForm.dev.js.map

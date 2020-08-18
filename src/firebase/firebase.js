15cff22035e9ceacbea4c0e983184a0dad92351e
Maxim <mreksha@gmail.com>
2020-08-18 13:07:15 +0300

    update

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyDJ16hOBUBDCRgsW9Y978BfW3ZiCPJffGk",
-    authDomain: "expensify-cb6ef.firebaseapp.com",
-    databaseURL: "https://expensify-cb6ef.firebaseio.com",
-    projectId: "expensify-cb6ef",
-    storageBucket: "expensify-cb6ef.appspot.com",
-    messagingSenderId: "498215442873",
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     appId: "1:498215442873:web:f94f3539ff883fdeec7e68",
     measurementId: "G-71WK4W4TZ2"
 };
